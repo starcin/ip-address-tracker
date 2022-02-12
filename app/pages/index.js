ce67bf5ba6b8770943a71df2558c7d1114d45a25
starcin <starcin@gmail.com>
2022-02-12 18:01:23 +0300

    Telefon yerleşimi eklendi

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,52 +1,70 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home(props) {
 	const [ipAdd, setIpAdd] = useState("")
 	const [ipData, setIpData] = useState()
 
-	const onIpSubmit = async (e) => {
-		e.preventDefault()
-		// console.log("submitted:", ipAdd)
+	async function fetchData() {
 		// Böyle adres sonuna '?' koyarak mı yapılıyor bu işler?
 		const response = await fetch(`/api/ip_data?ipAdd=${ipAdd}`, {
 			method: "GET",
 			headers: { "Content-Type": "application/json" },
 		})
 		setIpData(await response.json())
-		// console.log(api_key)
 	}
 
-	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<form className="form" onSubmit={onIpSubmit}>
-					<div className="form-control">
-						<label className="lbl">IP Address:</label>
-						<input
-							type="text"
-							placeholder="Leave empty for current IP"
-							onChange={(e) => setIpAdd(e.target.value)}
-						/>
-					</div>
-				</form>
-				{ipData && (
-					<div>
-						{/* {ipData} */}
-						<h1>{ipData.city}</h1>
-						<h2>{ipData.IPv4}</h2>
-					</div>
-				)}
+	const onIpSubmit = async (e) => {
+		e.preventDefault()
+		// console.log("submitted:", ipAdd)
+		fetchData()
+	}
 
-				{ipData && (
+	useEffect(fetchData, [])
+
+	return (
+		<div>
+			<main>
+				<h1>IP Address Tracker</h1>
+				<form className="form" onSubmit={onIpSubmit}>
+					<input
+						type="text"
+						placeholder={ipData ? ipData.IPv4 : "Leave empty for current IP"}
+						onChange={(e) => setIpAdd(e.target.value)}
+					/>
+					<input type="submit" value="&#9002;"></input>
+				</form>
+
+				{ipData ? (
 					<iframe
-						style={{ width: 100 + "%", height: 50 + "vh", border: 0 }}
+						// style={{ width: 100 + "%", height: 50 + "vh", border: 0 }}
 						loading="lazy"
 						src={`https://www.google.com/maps/embed/v1/view?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&center=${ipData.latitude},${ipData.longitude}&zoom=12`}
 						// src={`https://www.google.com/maps/embed/v1/view?key=${process.env.GOOGLE_MAPS_API_KEY}&center=39,37&zoom=12`}
 					></iframe>
+				) : (
+					<h2>... LOADING ...</h2>
+				)}
+				{ipData && (
+					<div className="info-box">
+						<div className="info-item">
+							<h2>IP ADDRESS </h2>
+							<p>{ipData.IPv4}</p>
+						</div>
+						<div className="info-item">
+							<h2>LOCATION</h2>
+							<p>
+								{ipData.city},{ipData.country_code} {ipData.postal}
+							</p>
+						</div>
+						<div className="info-item">
+							<h2>TIMEZONE</h2>
+							<p>-timezone-here-</p>
+						</div>
+						<div className="info-item">
+							<h2>ISP</h2>
+							<p>{ipData.IPv4}</p>
+						</div>
+					</div>
 				)}
 			</main>
 		</div>
