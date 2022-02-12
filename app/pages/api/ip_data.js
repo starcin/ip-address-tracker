686848566061b9a4349a7e2d3fd6bfd8b43c5524
starcin <starcin@gmail.com>
2022-02-12 20:04:18 +0300

    IP API tekrar değiştirildi

--- app/pages/api/ip_data.js
+++ app/pages/api/ip_data.js
@@ -6,8 +6,9 @@ export default async function handler(req, res) {
 		// if (req.query.ipAdd === "") {
 		// 	response = await fetch("https://geolocation-db.com/json")
 		// } else {
-		// response = await fetch(`https://geolocation-db.com/json/${req.query.ipAdd}`)
-		response = await fetch(`http://ip-api.com/json/${req.query.ipAdd}`)
+		// response = await fetch(`https://geolocation-db.com/json/${req.query.ipAdd}`)	// ISP vs. dönmüyor
+		// response = await fetch(`http://ip-api.com/json/${req.query.ipAdd}`)	// Şehir bilgisini yanlış dönüyor
+		response = await fetch(`http://ipwhois.app/json/${req.query.ipAdd}`)
 		// }
 		const data = await response.json()
 		res.status(200).json(data)
